686ac01985e9fb31a6598012a4b2a4616290e6e6
Hugo LIEGEARD <hugo@hl-media.co>
2025-02-26 16:40:35 +0100

    Travail du 26/02/2025

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    public function home(PostRepository $postRepository): Response
+    {
+        # Récupération de tous les articles
+        $posts = $postRepository->findAll();
+
+        # Affichage de la vue et le passage de la variable posts
+        return $this->render('default/home.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    # http://localhost:8000/category/politique
+        #[Route('/category/{slug}', name: 'default_category', methods: ['GET'])]
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        # Récupération de la catégorie
+        $category = $categoryRepository->findOneBy(['slug' => $slug]);
+
+        return $this->render('default/category.html.twig', [
+            'category' => $category
+        ]);
+    }
+
+    # http://localhost:8000/politique/monsuper-article_42.html
+    # cf. https://symfony.com/doc/current/routing.html#parameter-conversion
+    #[Route('/{category}/{slug}_{id}.html', name: 'default_post', methods: ['GET'])]
+    public function post(Post $post): Response
+    {
+        return $this->render('default/post.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    # TODO : Créer une page/route permettant de consulter les articles d'un auteur
+
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig');
+        #return new Response('Contact me!');
+    }
+
+    public function legal(): Response
+    {
+        return new Response('Mentions Légales!');
+    }
+
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        # Création d'un utilisateur
+        $user = new User();
+        $user->setEmail('hugo@actu360.com')
+            ->setPassword('demo')
+            ->setFirstname('Hugo')
+            ->setLastname('LIEGEARD')
+            ->setRoles(['ROLE_ADMIN']);
+
+        # Sauvegarde de l'utilisateur
+        $manager->persist($user);
+
+        # Création des categories
+        $politique = new Category();
+        $politique->setName('Politique')
+            ->setSlug('politique');
+
+        $economie = new Category();
+        $economie->setName('Economie')
+            ->setSlug('economie');
+
+        $culture = new Category();
+        $culture->setName('Culture')
+            ->setSlug('culture');
+
+        $sport = new Category();
+        $sport->setName('Sport')
+            ->setSlug('sport');
+
+        $sante = new Category();
+        $sante->setName('Santé')
+            ->setSlug('sante');
+
+        # Sauvegarde des éléments
+        $manager->persist($politique);
+        $manager->persist($economie);
+        $manager->persist($culture);
+        $manager->persist($sport);
+        $manager->persist($sante);
+
+        # Création d'un tableau de categories
+        $categories = [$politique, $economie, $culture, $sport, $sante];
+
+        # Création des articles
+        for ($i = 1; $i <= 50; $i++) {
+            $post = new Post();
+            $post->setTitle("Titre de l'article n°$i")
+                ->setSlug("titre-de-l-article-n-$i")
+                ->setContent("Contenu de l'article n°$i")
+                ->setImage("https://fakeimg.pl/600x400/?text=Actu360")
+                ->setCategory($categories[array_rand($categories)])
+                ->setUser($user);
+
+            $manager->persist($post);
+        }
+
+        # Déclenche l'enregistrement de toutes les données
+        $manager->flush();
+    }
+}
